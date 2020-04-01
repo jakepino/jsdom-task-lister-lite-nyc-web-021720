85541052f32eab352291a5dddf65a83138bc3fb9
github username <github email address>
2020-04-01 16:59:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	// your code here
+	const form = document.getElementById("new-task-description");
+	const tasks = document.getElementById("tasks");
+	document.addEventListener("submit", (event) => {
+		event.preventDefault();
+		task = form.value;
+		ele = document.createElement("li");
+		ele.innerHTML = `
+    <li>"${task}"</li>
+    <button type="button">Delete</button>
+    `;
+		tasks.append(ele);
+	});
+	document.addEventListener("click", (event) => {
+		// console.log(event.target.tagName);
+		if (event.target.tagName === "BUTTON") {
+			console.log(event.target.parentNode.parentNode.removeChild(ele));
+		}
+	});
 });
